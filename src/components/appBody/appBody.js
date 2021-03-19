d752fa418f6ca2a620eb5f4fdd1c460f45822617
Michal <michal@strangeflock.com>
2021-03-19 20:56:26 +0100

    fixed an issue with fav logic. Starting point for a modal view.

--- src/components/appBody/appBody.js
+++ src/components/appBody/appBody.js
@@ -2,25 +2,29 @@ import styles from './appBody.module.scss';
 import { observer } from 'mobx-react';
 import { useFilmsStore } from '../../store/filmsContext';
 import Card from '../card/card';
+import Modal from '../modal/Modal';
+import useModal from '../modal/useModal';
 
 const AppBody = observer(
     function AppBody(){
     
         const filmsStore = useFilmsStore()
-
-        function updateFavorite(target) {
-            console.log("update", target)
-
-        }
-
+        const {isShowing, toggle} = useModal();
+        
         return(
-            <div className={styles.body_grid}>
+            <div>
+                <button className="button-default" onClick={toggle}>Show Modal</button>
+                
+                <div className={styles.body_grid}>
                 {filmsStore.getAllFilms().map(item => (
                     
                     <Card key={item.id} object={item} filmsStore={filmsStore}/>
                     
                 ))}
+                </div>
+                <Modal isShowing={isShowing} hide={toggle} />
             </div>
+            
         )   
         
     }
