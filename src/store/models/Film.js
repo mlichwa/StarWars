7d0df251d0193bd0df83413488a0f26f505a0a6c
Michal <michal@strangeflock.com>
2021-03-21 20:34:07 +0100

    Adding file replacements

---
+++ src/store/models/Film.js
@@ -0,0 +1,32 @@
+import { flow, types } from 'mobx-state-tree';
+import LocalStorage from '../localStorage'
+
+const Film = types.model('Film', {
+    episode_id: types.identifierNumber,
+    title: types.maybeNull(types.string),
+    opening_crawl: types.maybeNull(types.string),
+    characters : types.optional(types.array(types.string), []),
+    director: types.maybeNull(types.string),
+    producer: types.maybeNull(types.string),
+    release_date: types.maybeNull(types.string),
+    isFaved: types.optional(types.boolean, false)
+})
+.actions(self => ({
+    
+    toggleFave: flow(function* () {
+        
+        console.log(self.episode_id);
+        self.isFaved = !self.isFaved
+
+        LocalStorage.updateFavoriteMovieForID(self.episode_id)
+
+    })
+    
+}))
+.views(self => ({
+    niceReleaseDate(){
+        return Date(self.release_date)
+    }
+}));
+
+export default Film
